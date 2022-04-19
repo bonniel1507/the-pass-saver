3715186c4fe6f7afbc34036b2b7fb81e7a787462
Bonnie Lei <bonniel1507@hstat.org>
2022-04-19 00:43:21 +0000

    up

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './scr/index.js', //may need two period the first part
+    entry: './src/index.js', //may need two period the first part
     output: {
         path: path.resolve(_dirname, 'dist'),
         filename: 'bundle.js'
